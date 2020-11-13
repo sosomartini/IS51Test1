a958c4c4b8442744b8f8315f112b6d85255455c0
Eseosa Otasowie <sosomartini@mail.fresnostate.edu>
2020-11-12 16:36:11 -0800

    completed first commit

---
+++ IS51_test1.py
@@ -0,0 +1,18 @@
+"""
+
+This program is design to determine which payment option is better (more money)
+1st option is $100 per day for 10 days. 
+2nd option is $1 per day and doubled each day for 10 days. 
+Two function would be set to calculate the pay rate.
+First function1 will calculate the amount for the first option, 
+Second function2 will calculate the amount for the second option.
+
+Function1 will output 100 * 10 days
+Function2 will loop 10 tries, with each time, doubling the amount and add the amount to the total.
+
+If the amount is equal, we output the user “option1 and option2 pays the same”
+If option1 is better, we output the user “option1 is better”
+If option2 is better, we output the user “option2 is better”
+
+
+"""
